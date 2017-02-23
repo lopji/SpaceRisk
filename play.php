98e9f27c0feba47f0b5b0f21ac13511151e3f909
JohnerPierre <pierre.johner@gmail.com>
2017-02-23 16:54:45 +0100

    i

--- play.php
+++ play.php
@@ -90,8 +90,9 @@
             </ol>
             <form class="form-inline">
                 <div class="form-group">
-                <input type="text" class="form-control" placeholder="Parlez ici..."/></div>
-                <button type="button" class="btn btn-primary btn-sm">Envoyer</button>
+                  <input type="text" class="form-control center-block" placeholder="Parlez ici..."/>
+                  <button type="button" class="btn btn-primary btn-sm ">Envoyer</button>
+                </div>
             </form>
           </div>
         <div class="sidebar-module">
