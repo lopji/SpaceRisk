0645f220fa2aca12054c8ed001f414c18557a1be
rocha <rocha@DESKTOP-P55TL95>
2017-03-09 09:26:53 +0100

    debug + correction

--- class/map/default.php
+++ class/map/default.php
@@ -6,6 +6,6 @@ return array(
     new Territory(0, array(1)),
     new Territory(1, array(0, 2, 3)),
     new Territory(2, array(1, 3)),
-    new Territory(3, array(1, 2))
+    new Territory(3, array(1, 2)) 
 );
 
